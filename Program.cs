7a30c38fc4dd579e00e5768f518e2f484b374230
Dimishack <dimishack.git@mail.ru>
2023-07-21 12:53:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeworkSeminar20072023
+{
+    internal class Program
+    {
+        static void Main()
+        {
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				throw;
+			}
+			finally
+			{
+                Console.WriteLine("\nНажмите любую клавишу для выхода из программы...");
+				Console.ReadKey(true);
+            }
+        }
+    }
+}
