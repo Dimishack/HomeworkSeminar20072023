ed122c04ffaffa55d949545a87920ba51650ad67
Dimishack <dimishack.git@mail.ru>
2023-07-21 13:34:53 +0300

    Выполнение задачи №64

--- Tasks/Task64.cs
+++ Tasks/Task64.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeworkSeminar20072023.Tasks
 {
@@ -17,7 +13,34 @@ namespace HomeworkSeminar20072023.Tasks
     {
         internal static void NumbersInDescending()
         {
+            uint maxNumber;
 
+            Console.WriteLine("Задача 64: Задайте значение N. Напишите программу, " +
+                "которая выведет все натуральные числа в промежутке от N до 1." +
+                " Выполнить с помощью рекурсии.\n" + new string('-', 30));
+            while (true)
+            {
+                Console.Write("Введите целое положительное число N для вывода чисел в промежутке от N до 1" +
+                    "\nN = ");
+                if (UInt32.TryParse(Console.ReadLine(), out var number))
+                {
+                    maxNumber = number;
+                    break;
+                }
+                Console.WriteLine("Ошибка: введенное значение не является целым положительным числом! Попробуйте еще раз");
+            }
+            Console.WriteLine("Вывод чисел от {0} до 1", maxNumber);
+            WriteNumbers(maxNumber);
+        }
+
+        private static void WriteNumbers(uint maxNumber)
+        {
+            if (maxNumber > 0)
+            {
+                Console.Write(maxNumber);
+                if (maxNumber > 1) Console.Write(", ");
+                WriteNumbers(maxNumber - 1);
+            }
         }
     }
 }
