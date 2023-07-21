2c28ec8d234ec0e9de917ce395fd4563bead8382
Dimishack <dimishack.git@mail.ru>
2023-07-21 17:42:44 +0300

    Меню, встречающее пользователя

--- Program.cs
+++ Program.cs
@@ -9,14 +9,39 @@ namespace HomeworkSeminar20072023
         {
 			try
 			{
-                Console.WriteLine("Задача №64");
-				Task64.NumbersInDescending();
-				WaitUser();
-                Console.WriteLine("Задача №66");
-				Task66.SumBethNumbers();
-				WaitUser();
-                Console.WriteLine("Задача №68");
-				Task68.FunctionAckerman();
+                ConsoleKey key;
+                Console.WriteLine("Добро пожаловать в программу с выполненными задачами №64, 66 и 68");
+                do
+                {
+                    Console.WriteLine("Выберите номер из списка с задачами, к которому Вы хотите перейти " +
+                        "\n(для выхода из программы нажмите <Enter>)");
+                    Console.WriteLine("1) Задача №64 \n2) Задача №66 \n3) Задача №68");
+                    key = Console.ReadKey(false).Key;
+                    Console.WriteLine();
+                    switch (key)
+                    {
+                        case ConsoleKey.Enter:
+                            break;
+                        case ConsoleKey.NumPad1:
+                        case ConsoleKey.D1:
+                            Task64.NumbersInDescending();
+                            WaitUser();
+                            break;
+                        case ConsoleKey.NumPad2:
+                        case ConsoleKey.D2:
+                            Task66.SumBethNumbers();
+                            WaitUser();
+                            break;
+                        case ConsoleKey.NumPad3:
+                        case ConsoleKey.D3:
+                            Task68.FunctionAckerman();
+                            WaitUser();
+                            break;
+                        default:
+                            Console.WriteLine("Ошибка: Данного номера с задачей нет! Выберите другой.");
+                            break;
+                    }
+                } while (key != ConsoleKey.Enter);
             }
 			catch (Exception ex)
 			{
